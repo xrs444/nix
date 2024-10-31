a17ef0036072086cd74228f6216a6f40b3f0dbf9
Thomas Letherby <xrs444@xrs444.net>
2024-10-30 20:47:11 -0700

    Update default.nix

--- nixos/default.nix
+++ nixos/default.nix
@@ -57,7 +57,7 @@
       [
         git
         nix-output-monitor
-      ];
+      ]
       ++ lib.optionals isInstall [
         inputs.determinate.packages.${platform}.default
         inputs.fh.packages.${platform}.default
