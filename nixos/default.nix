99a17924c2efa849be3ae16af5ab484e1f1bf50e
Thomas Letherby <xrs444@xrs444.net>
2024-11-01 10:18:50 -0700

    Update default.nix

--- nixos/default.nix
+++ nixos/default.nix
@@ -29,14 +29,8 @@
     overlays = [
       # Add overlays your own flake exports (from overlays and pkgs dir):
       outputs.overlays.additions
-      outputs.overlays.modifications
+#      outputs.overlays.modifications
       outputs.overlays.unstable-packages
-
-      # You can also add overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-      inputs.agenix.overlays.default
-      inputs.flypi.overlay
-      inputs.libations.overlays.default
 
       # Or just specify overlays directly here, for example:
       # (_: _: { embr = inputs.embr.packages."${pkgs.system}".embr; })
