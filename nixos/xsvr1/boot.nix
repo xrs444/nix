30a10f74b6a52a029a6396cf22f03f7b9af1816e
Thomas Letherby <xrs444@xrs444.net>
2024-11-26 12:09:37 -0700

    Update boot.nix

--- nixos/xsvr1/boot.nix
+++ nixos/xsvr1/boot.nix
@@ -25,61 +25,60 @@
     
   };
 
-}
 
-systemd.network = { 
+  systemd.network = { 
+    enable = true;
+    netdevs = {
+      "10G-Bond0" = {
+        netdevConfig = {
+          Kind = "bond";
+          Name = "bond0";
+        };
+        bondConfig = {
+          Mode = "802.3ad";
+          TransmitHashPolicy = "layer3+4";
+          MIIMonitorSec="0.100s";
+        };
+      };
+      "bond0.17" = {
+        netdevConfig = {
+          id = 17;
+          interface = "bond0";
+        };
+      };  
+      "bridge17" = {
+        netdevConfig = {
+          kind = "bridge";
+          Name = "br17";
+        };
+      };
+    };
 
-  enable = true;
-  netdevs = {
-    "10G-Bond0" = {
-      netdevConfig = {
-        Kind = "bond";
-        Name = "bond0";
+    networks = { 
+      "20-enp3s0f0" = {
+      matchConfig.Name = "enp3s0f0";
+      networkConfig.Bond = "bond0";
       };
-      bondConfig = {
-        Mode = "802.3ad";
-        TransmitHashPolicy = "layer3+4";
-        MIIMonitorSec="0.100s";
+      "20-enp3s0f1" = {
+      matchConfig.Name = "enp3s0f1";
+      networkConfig.Bond = "bond0";
       };
-    };
-    "bond0.17" = {
-      netdevConfig = {
-        id = 17;
-        interface = "bond0";
+      "30-bond0" = {
+        matchConfig.Name = "bond0";
+        linkConfig.RequiredForOnline = "carrier";
+        networkConfig.DHCP = "yes";
       };
-    };  
-    "bridge17" = {
-      netdevConfig = {
-        kind = "bridge";
-        Name "br17";
+      "40-eno1" = {
+         matchConfig.Name = "eno1";
+         linkConfig.RequiredForOnline = "carrier";
+         networkConfig.DHCP = "yes";
       };
-    };
-  };
-
-  networks = { 
-    "20-enp3s0f0" = {
-    matchConfig.Name = "enp3s0f0";
-    networkConfig.Bond = "bond0";
-    };
-    "20-enp3s0f1" = {
-    matchConfig.Name = "enp3s0f1";
-    networkConfig.Bond = "bond0";
-    };
-    "30-bond0" = {
-       matchConfig.Name = "bond0";
-       linkConfig.RequiredForOnline = "carrier";
-       networkConfig.DHCP = "yes";
-    };
-    "40-eno1" = {
-       matchConfig.Name = "eno1";
-       linkConfig.RequiredForOnline = "carrier";
-       networkConfig.DHCP = "yes";
-    };
-    "50-br17" = {
-      matchConfig = "br17";
-      bridgeConfig = {};
-      linkConfig = {
+      "50-br17" = {
+        matchConfig = "br17";
+        bridgeConfig = {};
+        linkConfig = {
         RequiredForOnline = "carrier";
+        };
       };
     };
   };
