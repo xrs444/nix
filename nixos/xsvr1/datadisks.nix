040695469673a032b010be33549c0d6e4d850ea2
Thomas Letherby <36782789+xrs444@users.noreply.github.com>
2024-11-26 10:31:37 -0700

    Update datadisks.nix

--- nixos/xsvr1/datadisks.nix
+++ nixos/xsvr1/datadisks.nix
@@ -1,15 +1,15 @@
-#
-#
-#
-#
-#
-#
+# ata-WDC_WD30EFRX-68EUZN0_WD-WCC4N0791190 - VDEV 1 - 1
+# ata-WDC_WD30EFRX-68EUZN0_WD-WCC4N0774016 - VDEV 1 - 2
+# ata-WDC_WD30EFRX-68EUZN0_WD-WCC4N0771782 - VDEV 2 - 1
+# ata-WDC_WD30EFRX-68EUZN0_WD-WMC4N2432718 - VDEV 2 - 2
+# ata-WDC_WD30EFRX-68EUZN0_WD-WMC4N2449151 - VDEV 3 - 1
+# ata-WDC_WD30EFRX-68EUZN0_WD-WCC4N0753744 - VDEV 3 - 2
 {
   disko.devices = {
     disk = {
       data-one = {
         type = "disk";
-        device = "/dev/disk/by-id/ata-";
+        device = "/dev/disk/by-id/ata-WDC_WD30EFRX-68EUZN0_WD-WCC4N0791190";
         content = {
           type ="gpt";
           zfs = {
@@ -26,7 +26,7 @@
     disk = {
       data-two = {
         type = "disk";
-        device = "/dev/disk/by-id/ata-";
+        device = "/dev/disk/by-id/ata-WDC_WD30EFRX-68EUZN0_WD-WCC4N0774016";
         content = {
           type ="gpt";
           zfs = {
@@ -43,7 +43,7 @@
     disk = {
       data-three = {
         type = "disk";
-        device = "/dev/disk/by-id/ata-";
+        device = "/dev/disk/by-id/ata-WDC_WD30EFRX-68EUZN0_WD-WCC4N0771782";
         content = {
           type ="gpt";
           zfs = {
@@ -60,7 +60,7 @@
     disk = {
       data-four = {
         type = "disk";
-        device = "/dev/disk/by-id/ata-";
+        device = "/dev/disk/by-id/ata-WDC_WD30EFRX-68EUZN0_WD-WMC4N2432718";
         content = {
           type ="gpt";
           zfs = {
@@ -77,7 +77,7 @@
     disk = {
       data-five = {
         type = "disk";
-        device = "/dev/disk/by-id/ata-";
+        device = "/dev/disk/by-id/ata-WDC_WD30EFRX-68EUZN0_WD-WMC4N2449151";
         content = {
           type ="gpt";
           zfs = {
@@ -94,7 +94,7 @@
     disk = {
       data-six = {
         type = "disk";
-        device = "/dev/disk/by-id/ata-";
+        device = "/dev/disk/by-id/ata-WDC_WD30EFRX-68EUZN0_WD-WCC4N0753744";
         content = {
           type ="gpt";
           zfs = {
