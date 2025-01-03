87e4c9620ce9ee509a6105b3a941b57f3a22327f
Thomas Letherby <36782789+xrs444@users.noreply.github.com>
2025-01-03 13:17:17 -0700

    Update disko.nix

--- nixos/common/base/disko.nix
+++ nixos/common/base/disko.nix
@@ -1,1 +1,14 @@
+{
+  inputs = {
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
+  nixosConfigurations.nixos = {
+    # ...
+    modules = [
+      ./configuration.nix
+      inputs.disko.nixosModules.disko
+    ];
+  };
+}
