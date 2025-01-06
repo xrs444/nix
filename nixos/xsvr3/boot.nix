f0d88e2606359df16dc48e29b4eff7ee717ffe2b
Thomas Letherby <36782789+xrs444@users.noreply.github.com>
2025-01-06 15:49:50 -0700

    Update boot.nix

--- nixos/xsvr3/boot.nix
+++ nixos/xsvr3/boot.nix
@@ -18,61 +18,70 @@
     
   };
 
-
-  systemd.network = { 
+  systemd.network = {
     enable = true;
     netdevs = {
-#      "10G-Bond0" = {
-#        netdevConfig = {
-#          Kind = "bond";
-#          Name = "bond0";
-#        };
-#        bondConfig = {
-#          Mode = "802.3ad";
-#          TransmitHashPolicy = "layer3+4";
-#          MIIMonitorSec="0.100s";
-#        };
-#      };
-#      "bond0.17" = {
-#        netdevConfig = {
-#          Kind = "vlan";
-#          id = 17;
-#          interface = "bond0";
-#          mode = "802.3ad";
-#        };
-#      };  
-#      "bridge17" = {
-#        netdevConfig = {
-#          kind = "bridge";
-#          Name = "br17";
-#        };
-#      };
-    };
-
-    networks = { 
-      "20-enp3s0f0" = {
-      matchConfig.Name = "enp3s0f0";
-      networkConfig.Bond = "bond0";
+      "Bond0" = {
+        netdevConfig = {
+          Kind = "bond";
+          Name = "bond0";
+        };
+        bondConfig = {
+          Mode = "802.3ad";
+          TransmitHashPolicy = "layer2+3";
+          MIIMonitorSec = "0.100s";
+          LACPTransmitRate = "fast";
+        };
       };
-      "20-enp3s0f1" = {
-      matchConfig.Name = "enp3s0f1";
-      networkConfig.Bond = "bond0";
+      "bond0.17" = {
+        netdevConfig = {
+          Kind = "vlan";
+          Name = "bond0.17";
+         };
+         vlanConfig.Id = 17;
+      };
+      "bridge17" = {
+        netdevConfig = {
+          Kind = "bridge";
+          Name = "bridge17";
+        };
+      };
+    };
+    networks = {
+      "20-enp1s0f0" = {
+        matchConfig.Name = "enp1s0f0";
+        networkConfig.Bond = "bond0";
+      };
+      "20-enp1s0f1" = {
+        matchConfig.Name = "enp1s0f1";
+        networkConfig.Bond = "bond0";
       };
       "30-bond0" = {
         matchConfig.Name = "bond0";
         linkConfig.RequiredForOnline = "carrier";
-        networkConfig.DHCP = "yes";
+        networkConfig = {
+          DHCP = "ipv4";
+          IPv6AcceptRA = true;
+        };
       };
-      "40-eno1" = {
-         matchConfig.Name = "eno1";
-         linkConfig.RequiredForOnline = "carrier";
-         networkConfig.DHCP = "yes";
+      "40-eno2" = {
+        matchConfig.Name = "eno2";
+        linkConfig.RequiredForOnline = "carrier";
+        networkConfig = {
+          DHCP = "ipv4";
+          IPv6AcceptRA = true;
+        };
       };
-      "50-br17" = {
-        matchConfig.Name = "br17";
+      # Added configuration for VLAN interface
+      "45-bond0.17" = {
+        matchConfig.Name = "bond0.17";
+        networkConfig.Bridge = "bridge17";
+      };
+      "50-bridge17" = {
+        matchConfig.Name = "bridge17";
         bridgeConfig = {};
         linkConfig = {
-        RequiredForOnline = "carrier";
+          RequiredForOnline = "carrier";
         };
       };
     };
