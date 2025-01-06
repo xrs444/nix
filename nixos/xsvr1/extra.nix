58cb2cd126e26536db30ced55bab50f2adc74591
Thomas Letherby <36782789+xrs444@users.noreply.github.com>
2025-01-06 16:07:11 -0700

    Update extra.nix

--- nixos/xsvr1/extra.nix
+++ nixos/xsvr1/extra.nix
@@ -1,5 +1,6 @@
 _: {
-  imports = [ 
+  imports = [
+    ../common/base/serverdefaults.nix
     ../common/services/zfs/default.nix
     ../common/services/zfs/zfs-xsvr1.nix
 #    ../common/services/nfs/xsvr1.nix 
