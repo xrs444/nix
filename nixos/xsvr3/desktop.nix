1395c1a6c681cb1f1d4dc4c7d9d8c2a8bf3368f6
Thomas Letherby <36782789+xrs444@users.noreply.github.com>
2025-01-06 16:49:28 -0700

    Update desktop.nix

--- nixos/xsvr3/desktop.nix
+++ nixos/xsvr3/desktop.nix
@@ -1,17 +1,43 @@
 { config, pkgs, ... }:
 
 {
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.xkb = {
+  services.xserver = {
+  enable = true;
+  displayManager.gdm.enable = true;
+  desktopManager.gnome.enable = true;
+  xkb = {
     layout = "us";
     variant = "";
+    };
   };
-  services.xserver.libinput.enable = true;
-  programs.firefox.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
-}
+  libinput.enable = true;
+  programs = {
+    firefox.enable = true;
+    gnupg.agent = {
+      enable = true;
+       enableSSHSupport = true;
+     };
+   };
+  environment.gnome.excludePackages = (with pkgs; [
+    gnome-photos
+    gnome-tour
+    ]) ++ (with pkgs.gnome; [
+    cheese # webcam tool
+    gnome-music
+    epiphany # web browser
+    geary # email reader
+    evince # document viewer
+    gnome-characters
+    totem # video player
+    tali # poker game
+    iagno # go game
+    hitori # sudoku game
+    atomix # puzzle game
+    gnome-calculator
+    yelp # help viewer
+    gnome-maps
+    gnome-weather
+    gnome-contacts
+    simple-scan
+    ];
+  }
