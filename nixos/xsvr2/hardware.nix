a3fe6809c586134ec283d92dcba7eb010ca4a276
Thomas Letherby <xrs444@xrs444.net>
2025-01-07 14:19:57 -0700

    Update hardware.nix

--- nixos/xsvr2/hardware.nix
+++ nixos/xsvr2/hardware.nix
@@ -7,7 +7,7 @@
     inputs.nixos-hardware.nixosModules.common-cpu-intel
     inputs.nixos-hardware.nixosModules.common-pc-ssd
   ];
-  hardware.cpu.amd.updateMicrocode
+  hardware.cpu.amd.updateMicrocode = true;
   nixpkgs.hostPlatform = "x86_64-linux";
 
 
