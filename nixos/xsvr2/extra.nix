1b5f288789fe4d7ea3b15c77a8fe7caadfc0f5b7
Thomas Letherby <xrs444@xrs444.net>
2025-01-07 14:24:13 -0700

    Update extra.nix

--- nixos/xsvr2/extra.nix
+++ nixos/xsvr2/extra.nix
@@ -1,8 +1,9 @@
 _: {
   imports = [
     ../common/base/serverdefaults.nix
-    ../common/services/zfs.nix
-    ../common/services/nfs/xsvr1.nix
+    ../common/services/zfs/zfs.nix
+    ../common/services/zfs/zfs-xsvr2.nix
+#    ../common/services/nfs/xsvr2.nix
     ../common/services/openssh.nix    
     ];
 
