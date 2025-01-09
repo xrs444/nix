79a777ac9e9d232b400f98b928c26e59d9687216
Thomas Letherby <36782789+xrs444@users.noreply.github.com>
2025-01-09 09:32:08 -0700

    Update serverdefaults.nix

--- nixos/common/base/serverdefaults.nix
+++ nixos/common/base/serverdefaults.nix
@@ -5,4 +5,13 @@ _: {
     AllowHybridSleep=no
     AllowSuspendThenHibernate=no
   '';
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
+
+  boot.kernel.sysctl."vm.page-cluster" = 1;
+  zramSwap = {
+    algorithm = "lz4";
+    enable = true;
+  };
+
 }
