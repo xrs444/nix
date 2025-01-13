0fdcb421a6ccb3bcc02a9034030b655c4f3844d9
Thomas Letherby <xrs444@xrs444.net>
2025-01-13 10:59:32 -0700

    fixup users

---
+++ nixos/common/users/root/default.nix
@@ -0,0 +1,7 @@
+_: {
+  users.users.${username} = {
+    extraGroups = [
+      "wheel"
+    ];
+  };
+}
--- nixos/common/users/thomas-local/default.nix
+++ nixos/common/users/thomas-local/default.nix
@@ -1,16 +1,13 @@
-{ pkgs, config, ... }:
+{ pkgs, config, username... }:
 let
   ifExists = groups: builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
 in
 {
   users.users.thomas-local = {
-    isNormalUser = true;
     extraGroups =
       [
         "networkmanager"
-        "users"
         "wheel"
       ];
-    packages = with pkgs; [ home-manager ];
   };
 }
