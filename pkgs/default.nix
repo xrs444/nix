8ab07aac5eb8d39ed32d0eb464d4e1d728f8c589
Thomas Letherby <xrs444@xrs444.net>
2025-01-13 12:24:36 -0700

    fix pkgs?

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,10 +1,4 @@
-# Custom packages, that can be defined similarly to ones from nixpkgs
-# Build them using 'nix build .#example' or (legacy) 'nix-build -A example'
 
 {
-  pkgs ? (import ../nixpkgs.nix) { },
-}:
-{
-  ght = pkgs.callPackage ./ght { };
   nixfmt-plus = pkgs.callPackage ./nixfmt-plus.nix { };
 }
