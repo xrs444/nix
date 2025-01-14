e3a34574851fd21a5021c94a9d507fd757b9df39
Thomas Letherby <xrs444@xrs444.net>
2025-01-14 15:47:53 -0700

    Update default.nix

--- nixos/common/services/comin/default.nix
+++ nixos/common/services/comin/default.nix
@@ -4,26 +4,14 @@
   lib,
   inputs,
   ...
-}:
+}: 
 {
-  outputs = { self, nixpkgs, comin }: {
-    nixosConfigurations = {
-      hostname = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          comin.nixosModules.comin
-          ({...}: {
-            services.comin = {
-              enable = true;
-              remotes = [{
-                name = "origin";
-                url = "https://github.com/xrs444/nix.git";
-                branches.main.name = "main";
-              }];
-            };
-          })
-        ];
-      };
-    };
+  services.comin = {
+    enable = true;
+    remotes = [{
+      name = "origin";
+      url = "https://github.com/xrs444/nix.git";
+      branches.main.name = "main";
+    }];
   };
 }
