fe0e62579a49d25f7038fa6dab3bcda80dd5bfee
Thomas Letherby <xrs444@xrs444.net>
2025-01-24 15:27:36 -0700

    Update network.nix

--- nixos/xsvr3/network.nix
+++ nixos/xsvr3/network.nix
@@ -1,6 +1,5 @@
 { pkgs, lib, ... }:
 {
-
   networking.useNetworkd = true;
   
   systemd.network = {
@@ -14,25 +13,48 @@
         bondConfig = {
           Mode = "802.3ad";
           TransmitHashPolicy = "layer2+3";
-          MIIMonitorSec = "0.100s";
+          MIIMonitorSec = "100ms";
           LACPTransmitRate = "fast";
+          MinLinks = "1";
+          AdSelect = "bandwidth";
+          AllSlavesActive = "yes";
         };
       };
     };
     networks = {
       "20-enp1s0f0" = {
         matchConfig.Name = "enp1s0f0";
-        networkConfig.Bond = "bond0";
+        networkConfig = {
+          Bond = "bond0";
+          ActivationPolicy = "always-up";
+        };
+        linkConfig = {
+          RequiredForOnline = "enslaved";
+          WakeOnLan = "magic";
+        };
       };
       "30-enp1s0f1" = {
         matchConfig.Name = "enp1s0f1";
-        networkConfig.Bond = "bond0";
+        networkConfig = {
+          Bond = "bond0";
+          ActivationPolicy = "always-up";
+        };
+        linkConfig = {
+          RequiredForOnline = "enslaved";
+          WakeOnLan = "magic";
+        };
       };
       "40-bond0" = {
         matchConfig.Name = "bond0";
         networkConfig = {
           DHCP = "ipv4";
           IPv6AcceptRA = true;
+          RequiredForOnline = "yes";
+          ConfigureWithoutCarrier = true;
+        };
+        linkConfig = {
+          RequiredForOnline = "carrier";
+          MIIMonitorSec = "100ms";
         };
       };
     };
