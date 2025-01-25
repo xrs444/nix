1b26413fe0728b569c757fd0aaa3676eb8e460a0
Thomas Letherby <xrs444@xrs444.net>
2025-01-24 17:17:38 -0700

    Update network.nix

--- nixos/xsvr3/network.nix
+++ nixos/xsvr3/network.nix
@@ -1,62 +1,47 @@
 { pkgs, lib, ... }:
 {
+
   networking.useNetworkd = true;
   
   systemd.network = {
     enable = true;
     netdevs = {
-      "10-bond0" = {
-        netdevConfig = {
-          Kind = "bond";
-          Name = "bond0";
-        };
-        bondConfig = {
-          Mode = "802.3ad";
-          TransmitHashPolicy = "layer2+3";
-          MIIMonitorSec = "100ms";
-          LACPTransmitRate = "fast";
-          MinLinks = "1";
-          AdSelect = "bandwidth";
-          AllSlavesActive = "yes";
-        };
+    "10-bond0" = {
+      netdevConfig = {
+        Kind = "bond";
+        Name = "bond0";
+      };
+      bondConfig = {
+        Mode = "802.3ad";
+        TransmitHashPolicy = "layer2+3";
+        MIIMonitorSec = "0.100s";
+        LACPTransmitRate = "fast";
+      };
+    };
+    "20-wlo1" = {
+      netdevConfig = {
+        Kind = "wireless";
+        Name = "wlo10";
+        enable = false;
       };
     };
     networks = {
-      "20-enp1s0f0" = {
+      "30-enp1s0f0" = {
         matchConfig.Name = "enp1s0f0";
-        networkConfig = {
-          Bond = "bond0";
-          ActivationPolicy = "always-up";
-        };
-        linkConfig = {
-          RequiredForOnline = "enslaved";
-          WakeOnLan = "magic";
-        };
+        networkConfig.Bond = "bond0";
       };
-      "30-enp1s0f1" = {
+      "40-enp1s0f1" = {
         matchConfig.Name = "enp1s0f1";
-        networkConfig = {
-          Bond = "bond0";
-          ActivationPolicy = "always-up";
-        };
-        linkConfig = {
-          RequiredForOnline = "enslaved";
-          WakeOnLan = "magic";
-        };
+        networkConfig.Bond = "bond0";
       };
-      "40-bond0" = {
+      "50-bond0" = {
         matchConfig.Name = "bond0";
         networkConfig = {
           DHCP = "ipv4";
           IPv6AcceptRA = true;
-          RequiredForOnline = "yes";
-          ConfigureWithoutCarrier = true;
-        };
-        linkConfig = {
-          RequiredForOnline = "carrier";
-          MIIMonitorSec = "100ms";
         };
       };
+    };
     };
   };
 }
