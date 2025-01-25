bc706e378865fd7fc7ff1b8c50a2095a88da1c28
Thomas Letherby <xrs444@xrs444.net>
2025-01-24 17:49:12 -0700

    Update network.nix

--- nixos/xsvr3/network.nix
+++ nixos/xsvr3/network.nix
@@ -6,40 +6,41 @@
   systemd.network = {
     enable = true;
     netdevs = {
-    "10-bond0" = {
-      netdevConfig = {
-        Kind = "bond";
-        Name = "bond0";
-      };
-      bondConfig = {
-        Mode = "802.3ad";
-        TransmitHashPolicy = "layer2+3";
-        MIIMonitorSec = "0.100s";
-        LACPTransmitRate = "fast";
-      };
-    };
-    networks = {
-      "20-enp1s0f0" = {
-        matchConfig.Name = "enp1s0f0";
-        networkConfig = {
-          Bond = "bond0";
-          DHCP = "no";
-      };
-      "30-enp1s0f1" = {
-        matchConfig.Name = "enp1s0f1";
-        networkConfig = {
-          Bond = "bond0";
-          DHCP = "no";
-      };
-      };
-      "40-bond0" = {
-        matchConfig.Name = "bond0";
-        networkConfig = {
-          DHCP = "ipv4";
-          IPv6AcceptRA = true;
+      "10-bond0" = {
+        netdevConfig = {
+          Kind = "bond";
+          Name = "bond0";
+        };
+        bondConfig = {
+          Mode = "802.3ad";
+          TransmitHashPolicy = "layer2+3";
+          MIIMonitorSec = "0.100s";
+          LACPTransmitRate = "fast";
         };
       };
-    };
+      networks = {
+        "20-enp1s0f0" = {
+          matchConfig.Name = "enp1s0f0";
+          networkConfig = {
+            Bond = "bond0";
+            DHCP = "no";
+          };
+        };
+        "30-enp1s0f1" = {
+          matchConfig.Name = "enp1s0f1";
+          networkConfig = {
+            Bond = "bond0";
+            DHCP = "no";
+          };
+        };
+        "40-bond0" = {
+          matchConfig.Name = "bond0";
+          networkConfig = {
+            DHCP = "ipv4";
+            IPv6AcceptRA = true;
+          };
+        };
+      };
     };
   };
 }
