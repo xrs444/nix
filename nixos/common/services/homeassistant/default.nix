000eb14883e78ea0966ad25f3334ee1b5d49b8b8
Thomas Letherby <36782789+xrs444@users.noreply.github.com>
2025-03-06 11:33:32 -0700

    Update default.nix

--- nixos/common/services/homeassistant/default.nix
+++ nixos/common/services/homeassistant/default.nix
@@ -12,17 +12,6 @@ let
 in
 lib.mkIf (lib.elem "${hostname}" installOn) {
 
-
-  environment.systemPackages = with pkgs; [
-    qemu
-    virt-manager
-    quickemu
-  ];
-
-virtualisation.libvirtd = {
-  enable = true;
-  };
-
   networking.firewall.allowedTCPPorts = [
     5900 # VNC
     16509 # virt-manager
