20514c91a0152f5d52dd83b2a4d683b5e176961c
Thomas Letherby <36782789+xrs444@users.noreply.github.com>
2025-03-06 11:33:09 -0700

    Update default.nix

--- nixos/common/services/kvm/default.nix
+++ nixos/common/services/kvm/default.nix
@@ -1,1 +1,25 @@
+{
+  config,
+  hostname,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  installOn = [
+    "xsvr1"
+  ];
+in
+lib.mkIf (lib.elem "${hostname}" installOn) {
 
+  environment.systemPackages = with pkgs; [
+    qemu
+    virt-manager
+    quickemu
+  ];
+
+virtualisation.libvirtd = {
+  enable = true;
+  };
+
+}
