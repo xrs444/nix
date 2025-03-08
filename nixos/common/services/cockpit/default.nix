40b46fe537e478bf90f14077856e28e40b35b2d2
Thomas Letherby <xrs444@xrs444.net>
2025-03-08 16:28:12 -0700

    Create default.nix

---
+++ nixos/common/services/cockpit/default.nix
@@ -0,0 +1,19 @@
+{
+  config,
+  hostname,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  installOn = [
+    "xsvr1"
+  ];
+in
+lib.mkIf (lib.elem "${hostname}" installOn) {
+
+services.cockpit = {
+  enable = true;
+  openFirewall = true; 
+  };
+}
