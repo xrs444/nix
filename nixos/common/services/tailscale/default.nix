5cbcbf987f8e7ee95d6f8176502c33aad22baf16
Thomas Letherby <xrs444@xrs444.net>
2025-03-09 21:38:14 -0700

    Update default.nix

--- nixos/common/services/tailscale/default.nix
+++ nixos/common/services/tailscale/default.nix
@@ -8,28 +8,60 @@
   ...
 }:
 let
-  # Declare which hosts have Tailscale enabled.
-  installOn = [
+  tsClients = [
     "xsvr1"
     "xsvr2"
+  ];
+  tsExitnodes = [
     "xsvr3"
   ];
-  tsExitNodes = [
-    "xsvr3"
-  ];
-in
-lib.mkIf (lib.elem "${hostname}" installOn) {
-  environment.systemPackages = with pkgs; lib.optionals isWorkstation [ trayscale ];
 
-  services.tailscale = {
-    enable = true;
-    extraUpFlags = [
-      "--operator=${username}"
-    ] ++ lib.optional (lib.elem "${hostname}" tsExitNodes) "--advertise-exit-node";
-    extraSetFlags = [
-      "--operator=${username}"
-    ] ++ lib.optional (lib.elem "${hostname}" tsExitNodes) "--advertise-exit-node";
-    openFirewall = true;
-    useRoutingFeatures = "both";
-  };
+in
+{
+  config = lib.mkMerge [
+    
+    ( lib.mkIf (lib.elem "${hostname}" tsClients) {
+
+      environment.systemPackages = with pkgs; lib.optionals isWorkstation [ trayscale ];
+      services.tailscale = {
+        enable = true;
+        extraUpFlags = [
+          "--operator=${username}"
+        ];
+        extraSetFlags = [
+          "--operator=${username}"
+        ];
+      };     
+    })   
+    ( lib.mkIf (lib.elem "${hostname}" tsExitNodes) {
+     
+      environment.systemPackages = with pkgs; [ 
+        ethtool
+        networkd-dispatcher
+      ];
+
+      services.tailscale = {
+        enable = true;
+        extraUpFlags = [
+          "--operator=${username} --advertise-exit-node"
+          ];
+        extraSetFlags = [
+          "--operator=${username} --advertise-exit-node"
+          ];
+        openFirewall = true;
+        useRoutingFeatures = "both";
+        interfaceName = "userspace-networking";
+        };
+     
+      services.networkd-dispatcher = {
+        enable = true;
+        rules."50-tailscale" = {
+        onState = ["routable"];
+        script = ''
+          "${pkgs.ethtool} NETDEV=$(ip -o route get 8.8.8.8 | cut -f 5 -d " ") | -K enp5s0 rx-udp-gro-forwarding on rx-gro-list off
+          '';
+         };
+      };  
+    })
+  ];
 }
