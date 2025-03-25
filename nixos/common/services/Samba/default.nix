c86962e70b2ef7531a6c9c6ef29d10080fe13abb
Thomas Letherby <xrs444@xrs444.net>
2025-03-25 16:19:16 -0700

    Sops and initial samba

---
+++ nixos/common/services/Samba/default.nix
@@ -0,0 +1,53 @@
+{
+  config,
+  hostname,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  installOn = [
+    "xsvr1"
+    "xsvr2"
+    "xsvr3"
+  ];
+
+in
+{
+
+  config = lib.mkMerge [
+    
+    ( lib.mkIf (lib.elem "${hostname}" installOn) {
+
+      services.samba = {
+        enable = true;
+        securityType = "user";
+        openFirewall = true;
+        settings = {
+          global = {
+            "workgroup" = "xrs444";
+            "server string" = "smbnix";
+            "netbios name" = "smbnix";
+            "security" = "user";
+            #"use sendfile" = "yes";
+            #"max protocol" = "smb2";
+            # note: localhost is the ipv6 localhost ::1
+            "hosts allow" = "172.16.0.0/12 127.0.0.1 localhost";
+            "hosts deny" = "0.0.0.0/0";
+            "guest account" = "nobody";
+            "map to guest" = "bad user";
+          };
+        };
+      }
+
+      services.samba-wsdd = {
+        enable = true;
+        openFirewall = true;
+      };
+
+      networking.firewall.enable = true;
+      networking.firewall.allowPing = true;
+
+    })
+  ];
+}
