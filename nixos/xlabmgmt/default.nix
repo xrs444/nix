548b3c9fe8e9b8a28c608c0e58071fd798a98133
Thomas Letherby <36782789+xrs444@users.noreply.github.com>
2025-03-26 09:37:19 -0700

    Update default.nix

--- nixos/xlabmgmt/default.nix
+++ nixos/xlabmgmt/default.nix
@@ -6,3 +6,29 @@
   username,
   ...
 }:
+{
+  imports = [
+    inputs.nixos-hardware.nixosModules.common-cpu-amd
+    inputs.nixos-hardware.nixosModules.common-pc-ssd
+    inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
+    inputs.nixos-hardware.nixosModules.common-gpu-amd
+    inputs.nixos-hardware.nixosModules.common-pc
+    ./disks.nix
+    ./network.nix
+  ];
+
+  nixpkgs.hostPlatform = "x86_64-linux";
+  boot = {
+    loader.systemd-boot.enable = true;
+    initrd = {
+      availableKernelModules = [
+        "mpt3sas"
+        "xhci_pci"
+        "ahci"
+        "usbhid"
+        "usb_storage"
+        "sd_mod"
+      ];
+    };
+  };  
+}
