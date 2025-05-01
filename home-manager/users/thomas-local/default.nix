8e840a34dc511eca1a8dfead7988ba6e16ec2239
Thomas Letherby <36782789+xrs444@users.noreply.github.com>
2025-05-01 15:09:09 -0700

    Update default.nix

--- home-manager/users/thomas-local/default.nix
+++ home-manager/users/thomas-local/default.nix
@@ -1,9 +1,4 @@
 { pkgs, ... }: {
-
-  home.username = "thomas-local";
-  home.homeDirectory = "/home/thomas-local";
-  
-  home.stateVersion = "24.11";
   
   home.packages = with pkgs; [
     # Add your user packages here
