2ae909d7da49b70fa71ee6d5a8ede32c3ab859fc
Thomas Letherby <36782789+xrs444@users.noreply.github.com>
2025-05-01 15:22:53 -0700

    Update default.nix

--- home-manager/users/xrs444/default.nix
+++ home-manager/users/xrs444/default.nix
@@ -4,11 +4,14 @@
     # Add your user packages here
   ];
   
-  programs.home-manager.enable = true;
-  programs.git = {
-    enable = true;
-    userName = "Thomas Letherby";
-    userEmail = "xrs444@xrs444.net";
+  programs = {
+    home-manager.enable = true;
+    git = {
+      enable = true;
+      userName = "Thomas Letherby";
+      userEmail = "xrs444@xrs444.net";
+    };
+    starship.enable = true;
   };
 
 }
