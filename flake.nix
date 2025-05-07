55c2f427fb630a017ac408251ea27fd11193c39b
Thomas Letherby <xrs444@xrs444.net>
2025-05-07 10:22:35 -0700

    zfs for xsvr2

--- flake.nix
+++ flake.nix
@@ -7,8 +7,8 @@
     fh.url = "https://flakehub.com/f/DeterminateSystems/fh/0";
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    nixpkgs.url = "https://flakehub.com/f/nixos/nixpkgs/0.2411.*";
-    nixpkgs-unstable.url = "https://flakehub.com/f/DeterminateSystems/nixpkgs-weekly/0";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-24.11";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     disko.url = "https://flakehub.com/f/nix-community/disko/1.10.0.tar.gz";
