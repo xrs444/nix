696ad70f9d49b2d33b26dd79f09ca4583ba3aff0
Thomas Letherby <36782789+xrs444@users.noreply.github.com>
2025-05-13 16:17:37 -0700

    Create default.nix

---
+++ nixos/xdt1-t/default.nix
@@ -0,0 +1,53 @@
+{
+  hostname,
+  inputs,
+  lib,
+  pkgs,
+  username,
+  ...
+}:
+{
+  imports = [
+    inputs.nixos-hardware.nixosModules.common-cpu-amd
+    inputs.nixos-hardware.nixosModules.common-pc-ssd
+    inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
+    inputs.nixos-hardware.nixosModules.common-gpu-nvidia
+    inputs.nixos-hardware.nixosModules.common-pc
+    ./disks.nix
+    ./network.nix
+  ];
+  hardware.cpu.amd.updateMicrocode = true;
+  nixpkgs.hostPlatform = "x86_64-linux";
+
+  boot = {
+    loader = {
+      systemd-boot.enable = false;
+      grub = {
+        enable = true;
+        device = "nodev";
+        useOSProber = true;
+        efiSupport = true;
+      };
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
+      };
+    };
+    initrd = {
+      availableKernelModules = [
+        "xhci_pci"
+        "ahci"
+        "usbhid"
+        "usb_storage"
+        "sd_mod"
+        "nvme"
+      ];
+      kernelModules = [
+        "kvm-amd"
+      ];
+    };
+  };
+
+  powerManagement.cpuFreqGovernor = "performance";
+  
+}
