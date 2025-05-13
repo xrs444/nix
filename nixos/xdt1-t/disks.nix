67ff09cf57fc4ccbf120acd274fe91096c5142fd
Thomas Letherby <36782789+xrs444@users.noreply.github.com>
2025-05-13 16:20:51 -0700

    Update disks.nix

--- nixos/xdt1-t/disks.nix
+++ nixos/xdt1-t/disks.nix
@@ -1,1 +1,38 @@
-{}
+# - System
+# - Scratch
+
+{ lib, ... }:
+
+{
+  disko.devices = {
+    disk = {
+      main = {
+        device = "/dev/disk/by-id/";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "4000M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            root = {
+              size = "50%";
+              content = {
+                type = "filesystem";
+                format = "xfs";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
