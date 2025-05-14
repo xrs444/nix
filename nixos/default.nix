174085826c6b4024e3670dfbb9b1f25cb49d8628
Thomas Letherby <xrs444@xrs444.net>
2025-05-14 08:20:36 -0700

    Update default.nix

--- nixos/default.nix
+++ nixos/default.nix
@@ -55,7 +55,7 @@
       # Add overlays your own flake exports (from overlays and pkgs dir):
 #      outputs.overlays.additions
 #      outputs.overlays.modifications
-      outputs.overlays.unstable-packages
+#      outputs.overlays.unstable-packages
 
       # Or just specify overlays directly here, for example:
       # (_: _: { embr = inputs.embr.packages."${pkgs.system}".embr; })
