efac6d16864e6354c09921e7a561074bd3904508
Thomas Letherby <xrs444@xrs444.net>
2025-06-17 20:12:16 -0700

    Update default.nix

--- nixos/common/services/ffr-keepalived/default.nix
+++ nixos/common/services/ffr-keepalived/default.nix
@@ -58,11 +58,15 @@ else
         !
         router bgp ${toString frrASN}
          bgp router-id ${currentNode.routerId}
+         bgp listen range 172.20.3.0/24 peer-group CILIUM
+         !
          neighbor CILIUM peer-group
          neighbor CILIUM remote-as ${toString ciliumASN}
-         neighbor 172.20.3.10 peer-group CILIUM
-         neighbor 172.20.3.20 peer-group CILIUM
-         neighbor 172.20.3.30 peer-group CILIUM
+         neighbor CILIUM activate
+         neighbor CILIUM soft-reconfiguration inbound
+         neighbor CILIUM timers 3 9
+         neighbor CILIUM timers connect 15
+         neighbor CILIUM ebgp-multihop 4
          !
          address-family ipv4 unicast
            redistribute connected
