1d2de6cc64d2456d10e563d425d3714736eeeed6
Thomas Letherby <xrs444@xrs444.net>
2025-06-20 12:21:59 -0700

    Update default.nix

--- nixos/common/services/ffr-keepalived/default.nix
+++ nixos/common/services/ffr-keepalived/default.nix
@@ -33,7 +33,7 @@ let
   ciliumASN = 65001;
   ciliumIPs = [ "172.20.3.10" "172.20.3.20" "172.20.3.30" ];
   vipAddress = "172.20.3.200";
-  gatewayVipAddress = "172.20.1.200";
+  gatewayVipAddress = "172.20.1.101";
 
   # List of all node IPs
   allNodeIPs = map (node: node.ip) (lib.attrValues nodeConfigs);
