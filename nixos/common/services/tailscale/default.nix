28d9e664779ede35c15aa4ea3f5c8a443fa88426
Thomas Letherby <xrs444@xrs444.net>
2025-06-24 14:34:28 -0700

    Update default.nix

--- nixos/common/services/tailscale/default.nix
+++ nixos/common/services/tailscale/default.nix
@@ -46,25 +46,26 @@ in
       services.tailscale = {
         enable = true;
         extraUpFlags = [
-          "--operator=${username}"
+#          "--operator=${username}"
           "--advertise-exit-node"
           "--accept-routes"
           "--allow-lan-access"
+          "--advertise-routes=172.16.0.0/12"
           ];
         openFirewall = true;
         useRoutingFeatures = "both";
-        interfaceName = "userspace-networking";
+#        interfaceName = "userspace-networking";
         };
       networking.firewall.checkReversePath = "loose";
-      services.networkd-dispatcher = {
-        enable = true;
-        rules."50-tailscale" = {
-        onState = ["routable"];
-        script = ''
-          "${pkgs.ethtool} NETDEV=$(ip -o route get 8.8.8.8 | cut -f 5 -d " ") | -K enp5s0 rx-udp-gro-forwarding on rx-gro-list off
-          '';
-         };
-      };  
+#      services.networkd-dispatcher = {
+#        enable = true;
+#        rules."50-tailscale" = {
+#        onState = ["routable"];
+#        script = ''
+#         "${pkgs.ethtool} NETDEV=$(ip -o route get 8.8.8.8 | cut -f 5 -d " ") | -K enp5s0 rx-udp-gro-forwarding on rx-gro-list off
+#          '';
+#         };
+#      };  
     })
   ];
 }
