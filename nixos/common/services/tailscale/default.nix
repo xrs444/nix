725453952d476e26ff38fa350ce54fb80cc2558d
Thomas Letherby <xrs444@xrs444.net>
2025-06-25 11:02:53 -0700

    Update default.nix

--- nixos/common/services/tailscale/default.nix
+++ nixos/common/services/tailscale/default.nix
@@ -36,36 +36,59 @@ in
     })   
     ( lib.mkIf (lib.elem "${hostname}" tsExitNodes) {
      
-      environment.systemPackages = with pkgs; [ 
-        tailscale
-        ethtool
-      ];
+      containers.nextcloud = {
+        autoStart = true;
+        hostName = "${hostname}-ts";
+        privateNetwork = false;
+        bridge = "bridge21";
+        config = { config, pkgs, lib, ... }: {
 
-      services.tailscale = {
-        enable = true;
-        extraUpFlags = [
-          "--advertise-exit-node"
-          "--accept-routes"
-          "--advertise-routes=172.16.0.0/12"
-          "--snat-subnet-routes=false"
+          environment.systemPackages = with pkgs; [
+            tailscale
+            ethtool
           ];
-        openFirewall = true;
-        useRoutingFeatures = "both";
-        };
-      networking.firewall.checkReversePath = "loose";
 
-      services.networkd-dispatcher = {
-        enable = true;
-        script = ''
-          #!/bin/sh
-          ethtool -K bond0 rx-udp-gro-forwarding on rx-gro-list off
-        '';
-     };
+          services.tailscale = {
+            enable = true;
+            extraUpFlags = [
+              "--advertise-exit-node"
+              "--accept-routes"
+              "--advertise-routes=172.16.0.0/12"
+              "--snat-subnet-routes=false"
+            ];
+            openFirewall = true;
+            useRoutingFeatures = "both";
+          };
+          networking.firewall.checkReversePath = "loose";
+
+          services.networkd-dispatcher = {
+            enable = true;
+            script = ''
+              #!/bin/sh
+              ethtool -K bond0 rx-udp-gro-forwarding on rx-gro-list off
+            '';
+          };
+
+          system.stateVersion = "25.05";
+
+          networking = {
+            useDHCP = true;
+            firewall = {
+              enable = false;
+            };
+            # Use systemd-resolved inside the container
+            # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
+            useHostResolvConf = lib.mkForce false;
+          };
+
+          services.resolved.enable = true;
+        };
+      };
     })
-};
-  
-  
-  
-  
-  
-  ];
+  ]
+}
+
+
+
+
+
