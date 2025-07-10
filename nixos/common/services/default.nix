625186070f48aab857d7a8fea813635ffc65c5b2
Thomas Letherby <xrs444@xrs444.net>
2025-07-10 11:26:11 -0700

    Update default.nix

--- nixos/common/services/default.nix
+++ nixos/common/services/default.nix
@@ -3,7 +3,9 @@ let
   currentDir = ./.;
   isDirectoryAndNotTemplate = name: type: type == "directory";
   directories = lib.filterAttrs isDirectoryAndNotTemplate (builtins.readDir currentDir);
-  importDirectory = name: import (currentDir + "/${name}") args;
+  importDirectory = name:
+    let mod = import (currentDir + "/${name}");
+    in if builtins.isFunction mod then mod args else mod;
 in
 {
   imports = lib.mapAttrsToList (name: _: importDirectory name) directories;
