0c476b8957216c1f62e537ba7ad96be76f7a0309
Thomas Letherby <xrs444@xrs444.net>
2025-07-09 22:48:54 -0700

    Update default.nix

--- nixos/common/services/default.nix
+++ nixos/common/services/default.nix
@@ -1,9 +1,9 @@
-{ lib, ... }:
+{ lib, ... } @ args:
 let
-  currentDir = ./.; # Represents the current directory
+  currentDir = ./.;
   isDirectoryAndNotTemplate = name: type: type == "directory";
   directories = lib.filterAttrs isDirectoryAndNotTemplate (builtins.readDir currentDir);
-  importDirectory = name: import (currentDir + "/${name}");
+  importDirectory = name: import (currentDir + "/${name}") args;
 in
 {
   imports = lib.mapAttrsToList (name: _: importDirectory name) directories;
