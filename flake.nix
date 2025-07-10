159450318dd5089c762c01ab896dd45833926de5
Thomas Letherby <xrs444@xrs444.net>
2025-07-10 10:22:43 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
   let
     inherit (self) outputs;
     stateVersion = "25.05";
-    lib = import ./lib { inherit inputs outputs stateVersion; };
+    lib = import ./lib { inherit inputs outputs stateVersion platform; };
   in
   {
     overlays = import ./overlays { inherit inputs; };
