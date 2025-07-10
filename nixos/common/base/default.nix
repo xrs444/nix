142e006efb46d4ba84e3b007e5a0e33491d6eb81
Thomas Letherby <xrs444@xrs444.net>
2025-07-10 10:09:26 -0700

    Update default.nix

--- nixos/common/base/default.nix
+++ nixos/common/base/default.nix
@@ -1,7 +1,8 @@
-{ lib, ... } @ args:
+{ config, lib, ... }@args:
 let
   currentDir = ./.;
-  isDirectoryAndNotTemplate = name: type: type == "directory";
+  isDirectoryAndNotTemplate = name: type:
+    type == "directory" && name != "templates";
   directories = lib.filterAttrs isDirectoryAndNotTemplate (builtins.readDir currentDir);
   importDirectory = name: import (currentDir + "/${name}") args;
 in
