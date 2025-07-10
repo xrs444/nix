09119be0d66ef630c94de80cefbc163d8556dbc3
Thomas Letherby <xrs444@xrs444.net>
2025-07-10 10:21:17 -0700

    Update default.nix

--- nixos/common/base/default.nix
+++ nixos/common/base/default.nix
@@ -1,11 +1,14 @@
 { lib, ... }:
 let
-  currentDir = ./.;
-  isDirectoryAndNotTemplate = name: type:
-    type == "directory" && name != "templates";
-  directories = lib.filterAttrs isDirectoryAndNotTemplate (builtins.readDir currentDir);
-  importDirectory = name: currentDir + "/${name}/default.nix";
+#  currentDir = ./.;
+#  isDirectoryAndNotTemplate = name: type:
+#    type == "directory" && name != "templates";
+#  directories = lib.filterAttrs isDirectoryAndNotTemplate (builtins.readDir currentDir);
+#  importDirectory = name: currentDir + "/${name}/default.nix";
+  defaultNixFiles = lib.filesystem.listFilesRecursive ./.;
+  moduleFiles = builtins.filter (file: lib.hasSuffix "/default.nix" file) defaultNixFiles;
 in
 {
-  imports = lib.mapAttrsToList (name: _: importDirectory name) directories;
+#  imports = lib.mapAttrsToList (name: _: importDirectory name) directories;
+  imports = moduleFiles;
 }
