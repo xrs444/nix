76a73794f0640eec3953361d71cffb0e4e5c5aef
Thomas Letherby <xrs444@xrs444.net>
2025-10-30 10:50:59 -0700

    Override kanidm package to use the latest 1.7.x version from unstable

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,22 +3,22 @@
   # Add custom packages from the 'pkgs' directory
   additions = final: _prev: import ../pkgs final.pkgs;
 
-  # Modify existing packages
-  modifications = final: prev: {
-    # Override kanidm to use the latest 1.7.x version
-    kanidm = prev.unstable.kanidm_1_7;
-    
-    # Example: override a package
-    # some-package = prev.some-package.overrideAttrs (old: rec {
-    #   # modifications
-    # });
-  };
-
-  # Access packages from nixpkgs-unstable
+  # Access packages from nixpkgs-unstable (this needs to come first)
   unstable-packages = final: _prev: {
     unstable = import inputs.nixpkgs-unstable {
       inherit (final) system;
       config.allowUnfree = true;
     };
+  };
+
+  # Modify existing packages (this depends on unstable-packages)
+  modifications = final: prev: {
+    # Override kanidm to use the latest 1.7.x version from unstable
+    kanidm = final.unstable.kanidm_1_7;
+    
+    # Example: override a package
+    # some-package = prev.some-package.overrideAttrs (old: rec {
+    #   # modifications
+    # });
   };
 }
