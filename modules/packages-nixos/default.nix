75a5b87da4a2863dd9efb857d2b7ff77ffed54b4
Thomas Letherby <xrs444@xrs444.net>
2025-11-03 13:17:22 -0700

    Refactor default.nix: remove unused functions and streamline directory imports

--- modules/packages-nixos/default.nix
+++ modules/packages-nixos/default.nix
@@ -1,11 +1,13 @@
 { config, lib, pkgs, ... }:
 
+let
+  currentDir = ./.; # Represents the current directory
+  isDirectoryAndNotTemplate = name: type: type == "directory" && name != "_template";
+  directories = lib.filterAttrs isDirectoryAndNotTemplate (builtins.readDir currentDir);
+  importDirectory = name: import (currentDir + "/${name}");
+in
 {
-  imports = [
-    ./cockpit
-    ./comin
-    ./tailscale
-  ];
+  imports = lib.mapAttrsToList (name: _: importDirectory name) directories;
 
   # NixOS-specific packages
   environment.systemPackages = with pkgs; [
