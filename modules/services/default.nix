e28b7360e8447de6628f2d760f8aa61d9e8fb99a
Thomas Letherby <xrs444@xrs444.net>
2025-11-03 12:10:23 -0700

    Refactor default.nix: improve directory filtering to exclude template directories

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -1,9 +1,10 @@
 { lib, ... }:
 let
-  currentDir = ./.;
+  currentDir = ./.; # Represents the current directory
   isDirectoryAndNotTemplate = name: type: type == "directory" && name != "_template";
   directories = lib.filterAttrs isDirectoryAndNotTemplate (builtins.readDir currentDir);
+  importDirectory = name: import (currentDir + "/${name}");
 in
 {
-  imports = lib.mapAttrsToList (name: _: currentDir + "/${name}") directories;
+  imports = lib.mapAttrsToList (name: _: importDirectory name) directories;
 }
