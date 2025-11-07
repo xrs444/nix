aa2e1d31f92a8c1b6d9ec26788d6f6ba522f46fc
Thomas Letherby <36782789+xrs444@users.noreply.github.com>
2025-11-07 12:14:19 -0700

    Refactor Nix overlay with lazyssh and kanidm updates
    Refactor the Nix overlay to include lazyssh and improve kanidm package management.

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,33 +2,35 @@
 let
   unstablePkgs = system: inputs.nixpkgs-unstable.legacyPackages.${system};
 in
-{
-  # This one brings our custom packages from the 'pkgs' directory
-  additions = final: _prev: import ../pkgs {pkgs = final;};
+[
+  # Add the custom lazyssh overlay as a function taking final and prev
+  (import ./lazyssh.nix)
 
-  # This one contains whatever you want to overlay
-  modifications = final: prev: {
+  # Overlay for kanidm-related package pinning/modification
+  (final: prev: {
     # Override default kanidm to use 1.7 from unstable
     kanidm = (unstablePkgs final.system).kanidm_1_7;
-    
+
     # kanidm_1_7 for all kanidm servers (from unstable)
     kanidm_1_7 = (unstablePkgs final.system).kanidm_1_7;
-    
+
     # kanidmWithSecretProvisioning_1_7 for provisioning server (xsvr1)
     kanidmWithSecretProvisioning_1_7 = (unstablePkgs final.system).kanidm_1_7.override {
       enableSecretProvisioning = true;
     };
-    
+
     # kanidm-provision CLI tool - separate package from unstable to get 1.7
     kanidm-provision = (unstablePkgs final.system).kanidm-provision;
-  };
+  })
 
-  # When applied, the unstable nixpkgs set (declared in the flake inputs) will
-  # be accessible through 'pkgs.unstable'
-  unstable-packages = final: _prev: {
+  # Overlay for importing custom packages from ../pkgs directory
+  (final: _prev: import ../pkgs { pkgs = final; })
+
+  # Provide unstable as pkgs.unstable if needed
+  (final: _prev: {
     unstable = import inputs.nixpkgs-unstable {
       system = final.system;
       config.allowUnfree = true;
     };
-  };
-}
+  })
+]
