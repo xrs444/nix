1f9d2cc14373b0f0b684153c9783811f3881ed4a
Thomas Letherby <36782789+xrs444@users.noreply.github.com>
2025-11-07 11:46:12 -0700

    Simplify kanidm configuration for Darwin
    Removed unnecessary client service settings for Darwin.

--- modules/packages-common/kanidm/default.nix
+++ modules/packages-common/kanidm/default.nix
@@ -5,22 +5,10 @@ let
   isDarwin = pkgs.stdenv.isDarwin;
   kanidmPackage = pkgs.kanidm;
 in
-lib.mkMerge [
-  # Install kanidm package everywhere (NixOS and Darwin)
+
   {
     environment.systemPackages = with pkgs; [
       kanidmPackage
     ];
-  }
+  };
 
-  # Only set up Kanidm client service settings on NixOS (NOT on Darwin)
-  (lib.mkIf (!isDarwin) {
-    services.kanidm = {
-      enableClient = true;
-      clientSettings = {
-        uri = kanidmServerUri;
-        verify_ca = true;
-      };
-    };
-  })
-]
