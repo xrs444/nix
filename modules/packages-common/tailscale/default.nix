336339d3a7362f1933f69c640cf1de570033f457
Thomas Letherby <36782789+xrs444@users.noreply.github.com>
2025-11-07 11:51:35 -0700

    Refactor Tailscale configuration for NixOS

--- modules/packages-common/tailscale/default.nix
+++ modules/packages-common/tailscale/default.nix
@@ -1,38 +1,27 @@
-{
-  config,
-  hostname,
-  isWorkstation,
-  lib,
-  pkgs,
-  username,
-  platform,
-  ...
-}:
+{ config, hostname, isWorkstation, lib, pkgs, username, platform, ... }:
 let
-  # Define which hosts should have Tailscale client functionality
+  isDarwin = pkgs.stdenv.isDarwin;
   tsClients = [ "xsvr1" "xsvr2" "xsvr3" "xtl1-t-nixos" "xlt1-t" ];
-  
-  # Common configuration for all Tailscale clients
   enableTailscale = lib.elem "${hostname}" tsClients;
-  
 in
 {
   config = lib.mkIf enableTailscale {
-    # Common Tailscale client configuration
-    services.tailscale = {
-      enable = true;
-      extraUpFlags = [
-        "--operator=${username}"
-        "--accept-routes"
-      ];
-      extraSetFlags = [
-        "--operator=${username}"
-        "--accept-routes"
-      ];
-    };
-    
-    # Add GUI client for workstations
-    environment.systemPackages = with pkgs; 
+    services.tailscale = lib.mkMerge [
+      { enable = true; }
+      # Only on NixOS hosts
+      (lib.mkIf (!isDarwin) {
+        extraUpFlags = [
+          "--operator=${username}"
+          "--accept-routes"
+        ];
+        extraSetFlags = [
+          "--operator=${username}"
+          "--accept-routes"
+        ];
+      })
+    ];
+
+    environment.systemPackages = with pkgs;
       lib.optionals isWorkstation [ trayscale ];
   };
 }
