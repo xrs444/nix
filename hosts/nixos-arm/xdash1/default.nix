570d97f4b300cbdc556a586022f9df78fef1d89c
Thomas Letherby <xrs444@xrs444.net>
2025-11-19 11:49:33 -0700

    Refactor default.nix to simplify imports and enhance readability

--- hosts/nixos-arm/xdash1/default.nix
+++ hosts/nixos-arm/xdash1/default.nix
@@ -1,61 +1,45 @@
 {
-  hostname,
-  inputs,
-  lib,
-  pkgs,
-  username,
-  platform,
-  ...
-}:
-{
-  imports = [
-    ../common/hardware-orangepi.nix
-    ../common/boot.nix
-    ./network.nix
-    "${inputs.nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
-    # Only import letsencrypt if not minimal
-    (lib.optional (!config.minimalImage) ../../../modules/services/letsencrypt)
-    # Add other heavy modules here as needed
-  ];
+  { config, lib, pkgs, ... }:
+  {
+    imports =
+      [
+        ../common/hardware-orangepi.nix
+        ../common/boot.nix
+        ./network.nix
+        "${pkgs.path}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+      ]
+      ++ lib.optional (!config.minimalImage) ../../../modules/services/letsencrypt;
 
-  networking.hostName = hostname;
+    networking.hostName = "xdash1";
 
-  # Explicitly disable ZFS for ARM board
-  boot.supportedFilesystems = lib.mkForce [ "ext4" "vfat" "xfs" ];
+    boot.supportedFilesystems = lib.mkForce [ "ext4" "vfat" "xfs" ];
 
-  environment.systemPackages = with pkgs; [
-    labwc
-    firefox
-  ];
+    environment.systemPackages = with pkgs; [
+      labwc
+      firefox
+    ];
 
-  # Define the kiosk user
-  users.users.xdash1 = {
-    isNormalUser = true;
-    description = "Dashboard Kiosk User";
-    extraGroups = [ "video" ];
-    home = "/home/xdash1";
-  };
+    users.users.xdash1 = {
+      isNormalUser = true;
+      description = "Dashboard Kiosk User";
+      extraGroups = [ "video" ];
+      home = "/home/xdash1";
+    };
 
-  # Enable graphics
-  hardware.graphics.enable = true;
-  
-  # Disable X server
-  services.xserver.enable = false;
+    hardware.graphics.enable = true;
+    services.xserver.enable = false;
 
-  # Cage kiosk mode
-  services.cage = {
-    enable = true;
-    user = "xdash1";
-    program = "${pkgs.firefox}/bin/firefox -kiosk -private-window https://hass.xrs444.net";
-  };
+    services.cage = {
+      enable = true;
+      user = "xdash1";
+      program = "${pkgs.firefox}/bin/firefox -kiosk -private-window https://hass.xrs444.net";
+    };
 
-  # Auto-login for kiosk
-  services.getty.autologinUser = "xdash1";
+    services.getty.autologinUser = "xdash1";
 
-  # SD image configuration
-  sdImage = {
-    compressImage = false;  # Set to true if you want .img.zst
-    expandOnBoot = true;    # Auto-expand to fill SD card
-  };
-}
+    sdImage = {
+      compressImage = false;
+      expandOnBoot = true;
+    };
+  }
 
