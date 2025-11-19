df86fd0f213098f281070adb01f0936f1ca30f75
Thomas Letherby <xrs444@xrs444.net>
2025-11-19 11:04:59 -0700

    Fix acme SSH key formatting by removing trailing newline

--- modules/services/letsencrypt/default.nix
+++ modules/services/letsencrypt/default.nix
@@ -74,7 +74,7 @@ in
     group = "acme";
     home = "/var/lib/acme";
     createHome = true;
-    openssh.authorizedKeys.keys = [ config.sops.secrets.acme_ssh_key ];
+    openssh.authorizedKeys.keys = [ (lib.strings.removeSuffix "\n" config.sops.secrets.acme_ssh_key) ];
   };
   users.groups.acme = {};
 
