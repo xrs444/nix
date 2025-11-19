d4e783c7eaf9ba4549a79980e994d4923dc8edc3
Thomas Letherby <xrs444@xrs444.net>
2025-11-19 14:48:40 -0700

    Remove commented-out configurations from xdash1 default and network files

--- hosts/nixos-arm/xdash1/default.nix
+++ hosts/nixos-arm/xdash1/default.nix
@@ -3,40 +3,40 @@
   imports = [
     ../../base-nixos.nix
     ../common/default.nix
-    ../common/hardware-orangepi.nix
-    ../common/boot.nix
-    ./network.nix
+#    ../common/hardware-orangepi.nix
+#    ../common/boot.nix
+#    ./network.nix
   ];
 
   networking.hostName = "xdash1";
 
-      boot.supportedFilesystems = [ "vfat" "ext4" ];
+#  boot.supportedFilesystems = [ "vfat" "ext4" ];
 
-    environment.systemPackages = with pkgs; [
-      labwc
-      firefox
-    ];
+#  environment.systemPackages = with pkgs; [
+#    labwc
+#    firefox
+#  ];
 
-    users.users.xdash1 = {
-      isNormalUser = true;
-      description = "Dashboard Kiosk User";
-      extraGroups = [ "video" ];
-      home = "/home/xdash1";
-    };
+#  users.users.xdash1 = {
+#    isNormalUser = true;
+#    description = "Dashboard Kiosk User";
+#    extraGroups = [ "video" ];
+#    home = "/home/xdash1";
+#  };
 
-    hardware.graphics.enable = true;
-    services.xserver.enable = false;
+#  hardware.graphics.enable = true;
+#  services.xserver.enable = false;
 
-    services.cage = {
-      enable = true;
-      user = "xdash1";
-      program = "${pkgs.firefox}/bin/firefox -kiosk -private-window https://hass.xrs444.net";
-    };
+#  services.cage = {
+#    enable = true;
+#    user = "xdash1";
+#    program = "${pkgs.firefox}/bin/firefox -kiosk -private-window https://hass.xrs444.net";
+#  };
 
-    services.getty.autologinUser = "xdash1";
+#  services.getty.autologinUser = "xdash1";
 
-    sdImage = {
-      compressImage = false;
-      expandOnBoot = true;
-    };
+#  sdImage = {
+#    compressImage = false;
+#    expandOnBoot = true;
+#  };
 }
