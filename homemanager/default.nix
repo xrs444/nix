3fa09eec8dc22e323c064e41cc946e39d8e83bf2
Thomas Letherby <xrs444@xrs444.net>
2025-11-28 11:18:19 -0700

    style: format default.nix for improved readability and consistency

--- homemanager/default.nix
+++ homemanager/default.nix
@@ -1,4 +1,5 @@
-{ config,
+{
+  config,
   desktop ? null,
   lib ? null,
   pkgs ? null,
@@ -7,26 +8,23 @@
   username,
   inputs,
   ...
-  }:
+}:
 {
   # Only import desktop configuration if the host is desktop enabled
   # Only import user specific configuration if they have bespoke settings
-  imports =
-    [
-      # inputs.catppuccin.homeManagerModules.catppuccin
-      ./common/shell
-    ]
-    ++ lib.optional (builtins.isString desktop) ./common/desktop
-    ++ lib.optional (builtins.pathExists (./. + "/common/users/${username}")) ./common/users/${username}
-    ++ lib.optional (builtins.pathExists (./. + "/users/${username}")) ./users/${username};
+  imports = [
+    # inputs.catppuccin.homeManagerModules.catppuccin
+    ./common/shell
+  ]
+  ++ lib.optional (builtins.isString desktop) ./common/desktop
+  ++ lib.optional (builtins.pathExists (./. + "/common/users/${username}")) ./common/users/${username}
+  ++ lib.optional (builtins.pathExists (./. + "/users/${username}")) ./users/${username};
 
   home = {
     inherit username;
     stateVersion = if stateVersion != null then stateVersion else "24.05";
     homeDirectory = lib.mkForce (
-      if pkgs.stdenv.isDarwin
-      then "/Users/${username}"
-      else "/home/${username}"
+      if pkgs.stdenv.isDarwin then "/Users/${username}" else "/home/${username}"
     );
     # Reverse scroll direction on macOS (disable "Natural" scrolling)
     activation.reverseScrollDirection = lib.mkIf pkgs.stdenv.isDarwin ''
