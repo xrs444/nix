5b90da131c94da885dea6ac6227171105b554118
Thomas Letherby <xrs444@xrs444.net>
2025-12-11 21:34:32 -0700

    all but vscode

--- overlays/all.nix
+++ overlays/all.nix
@@ -6,7 +6,7 @@
   # Error: path '/nix/store/zkab67l0irvrpdhbh0h52w035wb21bn5-nix-dev' is not valid
   # This is baked into revision 1778f178603ed65b4e4033c64f04ea51142ad6f6
   # Workaround: Use vscode-extensions from nixpkgs or wait for upstream fix
-  # (inputs.nix-vscode-extensions.overlays.default)
+  (inputs.nix-vscode-extensions.overlays.default)
   (import ./kanidm.nix { inherit inputs; })
   (import ./nodejs.nix { inherit inputs; })
   (import ./unstable.nix { inherit inputs; })
