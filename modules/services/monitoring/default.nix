8f79394450e4cd0b3914ce9917b1d95fccb8839f
Thomas Letherby <xrs444@xrs444.net>
2026-01-19 19:32:08 -0700

    prometheus listener

--- modules/services/monitoring/default.nix
+++ modules/services/monitoring/default.nix
@@ -22,15 +22,13 @@ in
   ];
 
   config = lib.mkIf enableMonitoring {
-    # Open firewall ports for Tailscale network
+    # Open firewall ports for monitoring services
     networking.firewall = lib.mkIf isMonitoringServer {
-      interfaces.tailscale0 = {
-        allowedTCPPorts = [
-          9090 # Prometheus
-          9093 # Alertmanager
-          3000 # Grafana
-        ];
-      };
+      allowedTCPPorts = [
+        9090 # Prometheus
+        9093 # Alertmanager
+        3000 # Grafana
+      ];
     };
   };
 }
