8fb3349555f5977e80e366ae6582e13899227ce6
Thomas Letherby <xrs444@xrs444.net>
2026-05-07 17:08:25 -0700

    Kandim version bump to 1.10

--- overlays/kanidm.nix
+++ overlays/kanidm.nix
@@ -1,16 +1,16 @@
 { inputs, ... }:
 (final: prev: {
-  kanidm = (inputs.nixpkgs-unstable.legacyPackages.${final.stdenv.hostPlatform.system}).kanidm_1_9;
+  kanidm = (inputs.nixpkgs-unstable.legacyPackages.${final.stdenv.hostPlatform.system}).kanidm_1_10;
   kanidm_1_7 =
-    (inputs.nixpkgs-unstable.legacyPackages.${final.stdenv.hostPlatform.system}).kanidm_1_9;
-  kanidm_1_4 = final.kanidm_1_9; # or final.kanidm if you want
-  kanidmWithSecretProvisioning_1_9 =
-    (inputs.nixpkgs-unstable.legacyPackages.${final.stdenv.hostPlatform.system}).kanidm_1_9.override
+    (inputs.nixpkgs-unstable.legacyPackages.${final.stdenv.hostPlatform.system}).kanidm_1_10;
+  kanidm_1_4 = final.kanidm_1_10; # or final.kanidm if you want
+  kanidmWithSecretProvisioning_1_10 =
+    (inputs.nixpkgs-unstable.legacyPackages.${final.stdenv.hostPlatform.system}).kanidm_1_10.override
       {
         enableSecretProvisioning = true;
       };
   # Version-agnostic alias - update version once in the overlay
-  kanidmWithSecretProvisioning = final.kanidmWithSecretProvisioning_1_9;
+  kanidmWithSecretProvisioning = final.kanidmWithSecretProvisioning_1_10;
   kanidm-provision =
     (inputs.nixpkgs-unstable.legacyPackages.${final.stdenv.hostPlatform.system}).kanidm-provision;
 })
